4711dfff978711dc62f2602e5df86ba6d16e1558
Diego Beras <60742181+diegoberas@users.noreply.github.com>
2021-12-03 20:13:05 -0400

    Console app 2 pedir datos por consola

--- Temperature/Program.cs
+++ Temperature/Program.cs
@@ -7,11 +7,34 @@ namespace Temperature
     {
         static void Main(string[] args)
         {
-            var t = new Temper();
-            Console.WriteLine()
-            string scale = t.Scale = "celsius";
-            double temp = t.TemperatureDigit = 5;
-            Console.WriteLine(Temper.ToKelvin(temp, scale));
+            Temper t = new Temper();
+            Console.WriteLine("Introduzca la temperatura: ");
+            double temp = t.TemperatureDigit = double.Parse(Console.ReadLine());
+            Console.WriteLine("Introduzca la escala [Kelvin] [Celcius] [Fahrenheit]: ");
+            string scale = t.Scale = Console.ReadLine().ToLower();
+            Console.Write("Convertir a [1]Kelvin [2]Celcius [3]Fahrenheit: ");
+            int answer = int.Parse(Console.ReadLine());
+
+            switch (answer)
+            {
+                case 1:
+                    Console.WriteLine("To Kelvin: " + Temper.ToKelvin(temp, scale));
+                    break;
+                case 2:
+                    Console.WriteLine("To Celsius: " + Temper.ToCelcius(temp, scale));
+                    break;
+                case 3:
+                    Console.WriteLine("To Fahrenheit: " + Temper.ToFahrenheit(temp, scale));
+                    break;
+
+                default:
+                    Console.WriteLine("Error. Intentalo de nuevo!");
+                    break;
+            }
+
+
+
+            
         }
     }
 }
